44ca94a882b23ce74d7825e066fb9880a7bcbed7
Michael J Kresca <Michael.Kresca@gmail.com>
2018-02-26 06:10:08 -0600

    test

--- TitleCountMapper.py
+++ TitleCountMapper.py
@@ -8,9 +8,12 @@ import string
 stopWordsPath = sys.argv[1]
 delimitersPath = sys.argv[2]
 
-
+input_lines = sys.stdin.readlines()
+print input_lines
 # TODO
 with open(stopWordsPath) as f:
+
+#.readlines()
     # TODO
 
 
