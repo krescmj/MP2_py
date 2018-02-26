6cf771ffb8cca52499bbd466e9844fd3d1cad8ff
Michael J Kresca <Michael.Kresca@gmail.com>
2018-02-26 06:39:48 -0600

    testing

--- TitleCountMapper.py
+++ TitleCountMapper.py
@@ -2,30 +2,39 @@
 
 import sys
 import string
-
-
+import re
+import collections
 
 stopWordsPath = sys.argv[1]
 delimitersPath = sys.argv[2]
 
-input_lines = sys.stdin.readlines()
-print input_lines
 # TODO
-with open(stopWordsPath) as f:
-
+#with open(stopWordsPath) as f:
+#	print input_lines
 #.readlines()
     # TODO
-
+def removeStopwords(wordlist, stopwords):
+    return [w for w in wordlist if w not in stopwords]
 
 
 
 
 
 #TODO 
-with open(delimitersPath) as f:
+#with open(delimitersPath) as f:
+regexPattern = '|'.join(map(re.escape, open(delimitersPath).readlines()))
     # TODO
 
 for line in sys.stdin:
-  
+	line = removeStopwords( re.split(regexPattern, line).lower(), open(delimitersPath).readlines())
+	
+	counts = collections.Counter(words).most_common()
+	
+	#result = dict()
+	#for x in range(0, len(counts)):
+	#	result.ap
+	
+	return counts
+	
     # TODO
 
