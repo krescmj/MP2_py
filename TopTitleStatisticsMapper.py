5e00faf28452d9e179c95a3000c0d69c9e93a9ae
krescmj <30279119+krescmj@users.noreply.github.com>
2018-02-27 15:57:35 -0600

    Update TopTitleStatisticsMapper.py

--- TopTitleStatisticsMapper.py
+++ TopTitleStatisticsMapper.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
 import sys
 
+sum = 0
+minimum = None
+maximum = 0
+size = 0
 
 for line in sys.stdin:
-    # TODO
+	
+	line = line.strip()
+	word, count = line.split('\t', 1)
+	
+	try:
+		count = int(count)
+	except ValueError:
+		continue
+	
+	sum += count
+	
+	if minimum == None:
+		minimum = count
+	else:
+		minimum = min(minimum,count)
+		
+	maximum = max(maximum,count)
+	size += 1
+	
+sys.stdout.write('%s\t%s\t%s\t%s\n' % (sum, minimum, maximum, size))
