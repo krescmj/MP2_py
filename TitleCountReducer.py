78f7251ca8a5e4b466bb618c97d68d0a78732943
Michael J Kresca <Michael.Kresca@gmail.com>
2018-02-27 05:42:00 -0600

    dsfsda

--- TitleCountReducer.py
+++ TitleCountReducer.py
@@ -2,10 +2,21 @@
 from operator import itemgetter
 import sys
 
-#TODO
+current_word = None
+current_count = 0
 
 # input comes from STDIN
 for line in sys.stdin:
-    # TODO
+	line = line.strip()
+	word, count = line.split('\t', 1)
+	
+	if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            sys.stdout.write('%s\t%s\n' % (current_word, current_count))
+        current_count = count
+        current_word = word
 
-# TODO
+if current_word == word:
+    sys.stdout.write('%s\t%s\n' % (current_word, current_count))
