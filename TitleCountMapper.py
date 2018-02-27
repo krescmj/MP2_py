9fa366f2a53d0fa1321bc5041e8c6907b7ff3336
krescmj <30279119+krescmj@users.noreply.github.com>
2018-02-27 09:36:40 -0600

    Update TitleCountMapper.py

--- TitleCountMapper.py
+++ TitleCountMapper.py
@@ -8,19 +8,30 @@ import collections
 stopWordsPath = sys.argv[1]
 delimitersPath = sys.argv[2]
 
+stopwords = list()
+with open(stopWordsPath) as f:
+	for line in f.readlines():
+		stopwords.append(line.strip())
+
+delimiters = list()
+with open(delimitersPath) as f:
+	for line in f.readlines():
+		for chr in line.strip():
+			delimiters.append(chr)
+
 def removeStopwords(wordlist, stopwords):
     return [w for w in wordlist if w not in stopwords]
 
-regexPattern = '|'.join(map(re.escape, open(delimitersPath).readlines()))
+regexPattern = '|'.join(map(re.escape, delimiters))
 
 all_words = list()
 
-for line in sys.stdin:
+for line in open(sys.argv[3]):
 	line = line.strip()
 	words = re.split(regexPattern, line)
-	all_words = all_words + (removeStopwords( [x.lower() for x in words], open(stopWordsPath).readlines()))
-	
-counts = collections.Counter(all_words).most_common()
+	all_words = all_words + (removeStopwords( [x.lower() for x in words], stopwords))
+
+counts = collections.Counter(all_words).most_common(10)
 	
 for item in counts:
 	sys.stdout.write('%s\t%s\n' % (item[0], item[1]))
