e430c36bdf94c64f5dba9f1cabc14de1fa0c9d48
krescmj <30279119+krescmj@users.noreply.github.com>
2018-02-27 16:26:54 -0600

    Update TopTitleStatisticsReducer.py

--- TopTitleStatisticsReducer.py
+++ TopTitleStatisticsReducer.py
@@ -1,11 +1,43 @@
 #!/usr/bin/env python
-import sys
+import sys, math
 
+sum = 0
+minimum = None
+maximum = 0
+size = 0
+variance = 0
 
-
-#TODO
-
+counts = list()
 for line in sys.stdin:
-    # TODO
+	
+	count = line.strip()
+	
+	try:
+		count = int(count)
+	except ValueError:
+		continue
+	
+	sum += count
+	
+	if minimum == None:
+		minimum = count
+	else:
+		minimum = min(minimum,count)
+		
+	maximum = max(maximum,count)
+	
+	counts.append(count) 
+	
+mean = sum/len(counts)
 
-#TODO
+var_sum = 0
+for count in counts:
+	var_sum += math.pow(count - mean, 2)
+	
+variance = var_sum/len(counts)
+
+sys.stdout.write('Mean\t%s\n' % (mean))
+sys.stdout.write('Sum\t%s\n' % (sum))
+sys.stdout.write('Min\t%s\n' % (minimum))
+sys.stdout.write('Max\t%s\n' % (maximum))
+sys.stdout.write('Var\t%s\n' % (variance))
