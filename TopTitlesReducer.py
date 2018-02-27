18a9115cc49ae3b838e7b747587814f78ee6832e
krescmj <30279119+krescmj@users.noreply.github.com>
2018-02-27 13:45:19 -0600

    Update TopTitlesReducer.py

--- TopTitlesReducer.py
+++ TopTitlesReducer.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python
-import sys
+import sys, operator
 
+current_word = None
+current_count = 0
+value_list = list()
 
-
-# input comes from STDIN
 for line in sys.stdin:
-    # TODO
+
+	line = line.strip()
+	word, count = line.split('\t', 1)
+	
+	try:
+		count = int(count)
+	except ValueError:
+		continue
+		
+	if current_word == word:
+		current_count += count
+	else:
+		if current_word:
+			value_list.append([current_word, current_count])
+		current_count = count
+		current_word = word
+		
+if current_word == word:
+	value_list.append([current_word, current_count])
+	
+value_list = sorted(value_list, key=operator.itemgetter(1,0))
+
+for item in value_list[-10:]:
+	sys.stdout.write('%s\t%s\n' % (item[0], item[1]))
