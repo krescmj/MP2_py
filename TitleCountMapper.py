e588b868517c6fad81b81050d6d55c08779208bd
Michael J Kresca <Michael.Kresca@gmail.com>
2018-02-27 06:57:29 -0600

    sdafsad

--- TitleCountMapper.py
+++ TitleCountMapper.py
@@ -8,22 +8,13 @@ import collections
 stopWordsPath = sys.argv[1]
 delimitersPath = sys.argv[2]
 
-# TODO
-#with open(stopWordsPath) as f:
-#	print input_lines
-#.readlines()
-    # TODO
 def removeStopwords(wordlist, stopwords):
     return [w for w in wordlist if w not in stopwords]
 
-#TODO 
-#with open(delimitersPath) as f:
 regexPattern = '|'.join(map(re.escape, open(delimitersPath).readlines()))
-    # TODO
 
 all_words = list()
-#test = "sdafsad,sdafsda"
-#for line in test:
+
 for line in sys.stdin:
 	line = line.strip()
 	words = re.split(regexPattern, line)
@@ -31,13 +22,6 @@ for line in sys.stdin:
 	
 counts = collections.Counter(all_words).most_common()
 	
-	#result = dict()
-	#for x in range(0, len(counts)):
-	#	result.ap
-	
 for item in counts:
 	sys.stdout.write('%s\t%s\n' % (item[0], item[1]))
-	#return counts
-	
-    # TODO
 
