d8ed7276c8c393cdc7299590a8727362756f3ce0
krescmj <30279119+krescmj@users.noreply.github.com>
2018-02-28 12:17:37 -0600

    Update LinkCountReducer.py

--- LinkCountReducer.py
+++ LinkCountReducer.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
+from operator import itemgetter
 import sys
 
-#TODO
+current_word = None
+current_count = 0
 
 # input comes from STDIN
 for line in sys.stdin:
-    # TODO
+	line = line.strip()
+	link, count = line.split('\t', 1)
+	
+	try:
+		count = int(count)
+	except ValueError:
+		continue
+	
+	if current_word == link:
+		current_count += count
+	else:
+		if current_word:
+			sys.stdout.write('%s\t%s\n' % (current_word, current_count))
+		current_count = count
+		current_word = link
 
-# TODO
+if current_word == link:
+    sys.stdout.write('%s\t%s\n' % (current_word, current_count))
