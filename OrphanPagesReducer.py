bc1c5bd95db1f05d604ceae6c25af90b38224fbd
Michael J Kresca <Michael.Kresca@gmail.com>
2018-03-02 06:34:03 -0600

    sadfdsa

--- OrphanPagesReducer.py
+++ OrphanPagesReducer.py
@@ -10,15 +10,12 @@ for line in sys.stdin:
 	marker, page = line.split('\t',1)
 	
 	if marker == 'o':
-#		if page not in norphan_set:
 		orphan_set.add(page)
 	elif marker == 'n':
-		#if page in orphan_set:
-		#	orphan_set.remove(page)
 		norphan_set.add(page)
 
 orphans = orphan_set - norphan_set
-orphans = sorted(orphan_set)
+orphans = sorted(orphans)
 
 for orphan in orphans:		
 	sys.stdout.write('%s\n' % (orphan))
